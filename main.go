dd143099a4d14b937c266e11315d9abd31177fc6
Grant Ammons <gammons@gmail.com>
2015-03-28 22:02:55 -0400

    got oauth working

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"draftmark"
 	db "draftmark/persistence"
+	//b64 "encoding/base64"
 	"encoding/json"
+	"fmt"
 	"github.com/go-martini/martini"
 	"github.com/joho/godotenv"
+	//"github.com/martini-contrib/sessions"
+	"golang.org/x/oauth2"
 	"log"
+	//"math/rand"
 	"net/http"
+	"os"
 	"strconv"
 )
 
@@ -15,17 +21,26 @@ var database = &db.Client{}
 var user = &db.User{}
 var sync *draftmark.Sync
 
-func setupDatabase() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+var conf *oauth2.Config
 
+func setupDatabase() {
 	database.InitDB()
 	database.Db.LogMode(true)
 	//resetDB()
 
 	database.Db.Where("email = ?", "gammons@gmail.com").First(&user)
+}
+
+func setupOauth() {
+	conf = &oauth2.Config{
+		ClientID:     os.Getenv("DROPBOX_KEY"),
+		ClientSecret: os.Getenv("DROPBOX_SECRET"),
+		RedirectURL:  "http://localhost:3000/redirect",
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://www.dropbox.com/1/oauth2/authorize",
+			TokenURL: "https://api.dropbox.com/1/oauth2/token",
+		},
+	}
 }
 
 func resetDB() {
@@ -50,6 +65,21 @@ func getNote(params martini.Params) string {
 	return database.GetNoteContents(noteId)
 }
 
+func oauthInit(res http.ResponseWriter, req *http.Request) {
+	url := conf.AuthCodeURL("state")
+	http.Redirect(res, req, url, 302)
+}
+
+func oauthRedirect(w http.ResponseWriter, r *http.Request) string {
+	code := r.URL.Query().Get("code")
+	tok, err := conf.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(tok.AccessToken)
+	return tok.AccessToken
+}
+
 func setupMartini() {
 	m := martini.Classic()
 	static := martini.Static("public", martini.StaticOptions{Fallback: "/index.html"})
@@ -58,12 +88,19 @@ func setupMartini() {
 		go sync.DoSync(*user, "/notes")
 	})
 	m.Get("/note/:id", getNote)
+	m.Get("/authorize", oauthInit)
+	m.Get("/redirect", oauthRedirect)
 	m.NotFound(static, http.NotFound)
 	m.Run()
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	setupDatabase()
+	setupOauth()
 	log.Println("creating new sync with ", user.DropboxAccessToken)
 	sync = draftmark.NewSync(user.DropboxAccessToken)
 	sync.DoLogging = true
