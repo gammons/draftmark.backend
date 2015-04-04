63c5214176046a3669dd320e911a5e7f80cc6076
Grant Ammons <gammons@gmail.com>
2015-04-04 08:44:01 -0400

    make the note view work

--- public/src/note.js.jsx
+++ public/src/note.js.jsx
@@ -4,20 +4,19 @@ var NoteView = React.createClass({
   },
   componentDidMount: function() {
     this.getData(function(data) {
-      this.setState({content: data.content});
+      this.setState({content: data});
     }.bind(this));
 
-    var channel = this.props.pusher.subscribe('updates');
-    channel.bind('update', function(data) {
-      this.getData(function(data) {
-        this.setState({content: data.content});
-      }.bind(this));
-    }.bind(this));
+    //var channel = this.props.pusher.subscribe('updates');
+    // channel.bind('update', function(data) {
+    //   this.getData(function(data) {
+    //     this.setState({content: data.content});
+    //   }.bind(this));
+    // }.bind(this));
   },
   getData: function(successFn) {
     $.ajax({
       url: "/notes/"+this.props.id+"/content.json",
-      dataType: 'json',
       success: successFn,
       error: function(xhr, status, err) {
         console.error(this.props.url, status, err.toString());
