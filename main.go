a426d200b75ec434c6d1b7158ee1ff08f86f5670
Grant Ammons <gammons@gmail.com>
2015-04-11 08:51:32 -0400

    get the webhook working

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"draftmark"
 	db "draftmark/persistence"
+	"encoding/json"
 	"fmt"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/sessions"
 	"github.com/joho/godotenv"
 	"github.com/julienschmidt/httprouter"
 	"github.com/unrolled/render"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
@@ -18,7 +20,7 @@ var database = &db.Client{}
 var user = &db.User{}
 var sync *draftmark.Sync
 var rndr = render.New()
-var store = sessions.NewCookieStore([]byte("something-very-secret"))
+var store = sessions.NewCookieStore([]byte("dingleton"))
 
 func setupDatabase() {
 	database.InitDB()
@@ -51,8 +53,25 @@ func getNote(res http.ResponseWriter, req *http.Request, ps httprouter.Params) {
 	fmt.Fprintf(res, database.GetNoteContents(currentUser(req), path))
 }
 
-func doSync(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-	go sync.DoSync(*currentUser(req), "/notes")
+type SyncUsers struct {
+	Delta struct {
+		Users []int `json:"users"`
+	} `json:"delta"`
+}
+
+func doSync(res http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	if challenge := req.URL.Query().Get("challenge"); challenge != "" {
+		fmt.Fprintf(res, challenge)
+		return
+	}
+	body, _ := ioutil.ReadAll(req.Body)
+	var syncUsers SyncUsers
+	json.Unmarshal(body, &syncUsers)
+	for _, userId := range syncUsers.Delta.Users {
+		var user = db.User{}
+		database.Db.Where("dropbox_user_id = ?", userId).First(&user)
+		go sync.DoSync(user, "/notes")
+	}
 }
 
 func AuthRequired(h httprouter.Handle) httprouter.Handle {
@@ -90,10 +109,11 @@ func setupNegroni() {
 	router := httprouter.New()
 	router.GET("/notes.json", AuthRequired(listNotes))
 	router.GET("/content/*filepath", AuthRequired(getNote))
-	router.GET("/sync", AuthRequired(doSync))
 
 	router.GET("/authorize", oauthInit)
 	router.GET("/redirect", oauthRedirect)
+	router.GET("/sync", doSync)
+	router.POST("/sync", doSync)
 
 	n.UseHandler(router)
 	n.Run(":3000")
