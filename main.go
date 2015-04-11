4c1174234c2bfb58299ffc4985abf92cec5674d8
Grant Ammons <gammons@gmail.com>
2015-04-11 09:13:09 -0400

    whoops

--- main.go
+++ main.go
@@ -117,7 +117,7 @@ func setupNegroni() {
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Info("Error loading .env file.  This might be ok though if we're in prod.")
+		log.Println("Error loading .env file.  This might be ok though if we're in prod.")
 	}
 	setupDatabase()
 	setupOauth()
